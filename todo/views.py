91338898437d36cdab7363ed0c21f632bd529e2f
EricBruch <eric.bruch@outlook.de>
2022-12-02 20:04:01 +0100

    add basic case to view rest access

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Todo
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+    permission_classes = []  # permissions.IsAuthenticated
