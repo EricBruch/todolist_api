d551392dd3e8c69875f04e611808c2d74cd27028
EricBruch <eric.bruch@outlook.de>
2022-12-04 15:36:05 +0100

    add a create method to api for creating a todo with post

--- todo/views.py
+++ todo/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from rest_framework import permissions
 from .models import Todo
 from .serializers import TodoSerializer
+from django.http import HttpResponse
+from django.core import serializers
 
 
 class TodoViewSet(viewsets.ModelViewSet):
@@ -14,3 +15,12 @@ class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo.objects.all()
     serializer_class = TodoSerializer
     permission_classes = []  # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(
+            title=self.request.POST['title'],
+            description=self.request.POST['description'],
+            user=self.request.user,
+        )
+        serialized_obj = serializers.serialize('json', [todo])
+        return HttpResponse(serialized_obj, content_type='application/json')
