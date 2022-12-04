aeb244807ae8462e3fc5ab2fd7b21e0016f217a0
EricBruch <eric.bruch@outlook.de>
2022-12-04 14:45:21 +0100

    serialize complex object

--- todo/serializers.py
+++ todo/serializers.py
@@ -1,8 +1,21 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
+from rest_framework.fields import CurrentUserDefault
 from .models import Todo
 
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
+
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    # user = CurrentUserDefault() # UserSerializer()
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault())
+
     class Meta:
         model = Todo
-        fields = ['id', 'title', 'description', 'created_at'] #, 'user'
+        fields = ['id', 'title', 'description',
+                  'created_at', 'user']  # , 'user'
