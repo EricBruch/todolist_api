c5ff6d074c0915db60bac85c6dc1cc36b67e2a51
EricBruch <eric.bruch@outlook.de>
2022-12-06 23:42:55 +0100

    add register and login page and views for them

--- todo/views.py
+++ todo/views.py
@@ -6,6 +6,8 @@ from .models import Todo
 from .serializers import TodoSerializer
 from django.http import HttpResponse
 from django.core import serializers
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
 
 class TodoViewSet(viewsets.ModelViewSet):
@@ -24,3 +26,28 @@ class TodoViewSet(viewsets.ModelViewSet):
         )
         serialized_obj = serializers.serialize('json', [todo])
         return HttpResponse(serialized_obj, content_type='application/json')
+
+
+def login_view(request):
+
+    if request.method == 'POST':
+        username, password = request.POST['username'], request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            login(request, user)
+            return render(request, 'login/login.html', {'successful': True, })
+        else:
+            return render(request, 'login/login.html', {'error': True, })
+
+    return render(request, 'login/login.html')
+
+
+def register_view(request):
+
+    if request.method == 'POST':
+        username, password, email = request.POST['username'], request.POST[
+            'password'], request.POST['email']
+        user = User.objects.create_user(username, email, password)
+        login(request, user)
+        return render(request, 'register/register.html', {'successful': True})
+    return render(request, 'register/register.html')
--- todolist/urls.py
+++ todolist/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include, path
 from rest_framework import routers
-from todo.views import TodoViewSet
+from todo.views import TodoViewSet, login_view, register_view
 
 router = routers.DefaultRouter()
 router.register(r'todos', TodoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('login/', login_view),
+    path('register/', register_view),
     path('admin/', admin.site.urls),
 ]
